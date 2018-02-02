54f54b9cae1c437def0596c63af8c5fbd2750ac7
User <ppkrauss@gmail.com>
2018-02-02 05:09:16 -0200

    rm domain and rename j_as_t() to as_table()

--- bag_as_schema.sql
+++ bag_as_schema.sql
@@ -12,15 +12,16 @@
 DROP SCHEMA IF EXISTS bag CASCADE;
 CREATE SCHEMA bag;
 
-CREATE DOMAIN jbag AS JSONB
-  CHECK(  VALUE IS NULL OR  jsonb_typeof(VALUE) IN ('object','null')  )
-;  -- empty bag is JSON-null, and SQL NULL is "undefined"
+-- -- -- -- -- --
 
+/**
+ * "Bag datatype" validator. With some flavors for bag-element-name syntax.
+ */
 CREATE or replace FUNCTION bag.valid(
-  jbag
-  ,p_not_emp boolean DEFAULT false
-  ,p_no_digits boolean DEFAULT false
-  ,p_lower boolean DEFAULT false
+  JSONb -- the JSON-BAG
+  ,p_not_emp boolean DEFAULT true     -- to avoid empty bag-element-name
+  ,p_no_digits boolean DEFAULT false  -- true to reduce namespace to nom-digit words.
+  ,p_lower boolean DEFAULT false      -- true to reduce namespace to lower case.
 ) RETURNS boolean AS $f$
   SELECT bool_and(    -- key condictions
     (NOT(p_not_emp) OR key!='') AND
@@ -29,8 +30,23 @@ CREATE or replace FUNCTION bag.valid(
     AND             -- multiplicity condictions:
     jsonb_typeof(value)='number' AND vtext~'^\d+$' AND vtext::int>0
   )
-  FROM (select *, value::text as vtext, jsonb_typeof(value) FROM jsonb_each($1)) t
+  FROM (
+    SELECT *, value::text as vtext, jsonb_typeof(value)
+    FROM jsonb_each($1)
+  ) t
 $f$ language SQL IMMUTABLE;
+
+/**
+ * Wrap for bag.valid(), check array of bags.
+ */
+CREATE or replace FUNCTION valid(
+  JSONb[] -- the array of JSON-BAGs
+  ,p_not_emp boolean DEFAULT true
+  ,p_no_digits boolean DEFAULT false
+  ,p_lower boolean DEFAULT false
+) RETURNS boolean AS $wrap$
+  SELECT bool_and( bag.valid(jb) ) FROM unnest($1) u(jb)
+$wrap$ language SQL IMMUTABLE;
 
 
 CREATE TABLE bag.ref (e text, m int);
@@ -51,14 +67,17 @@ $f$ language SQL IMMUTABLE;
 
 
 /**
- * Returns an array of JSONb-bag representations as an element-multiplicity table.
+ * Representing an array of bags as a ref-table. Side-effect: merge all bags.
  */
-CREATE FUNCTION bag.j_as_t(JSONb[]) RETURNS SETOF bag.ref AS $f$
+CREATE FUNCTION bag.as_table(JSONb[]) RETURNS SETOF bag.ref AS $f$
   SELECT e, (m#>>'{}')::int
 	FROM unnest($1) t(x), LATERAL jsonb_each(x) a(e,m)
 $f$ language SQL IMMUTABLE;
 
-CREATE FUNCTION bag.j_as_t(JSONb) RETURNS SETOF bag.ref AS $f$
+/**
+ * Representing bag elements as rows in a ref-table.
+ */
+CREATE FUNCTION bag.as_table(JSONb) RETURNS SETOF bag.ref AS $f$
 	-- TO DO: must handle jsonb_typeof($1)='array'
   SELECT key, (value#>>'{}')::int FROM jsonb_each($1) a
 $f$ language SQL IMMUTABLE;
@@ -70,17 +89,16 @@ $f$ language SQL IMMUTABLE;
 CREATE FUNCTION bag.scaled_by(JSONb,int) RETURNS JSONb AS $f$
 	SELECT jsonb_object_agg(e,m)
 	FROM (
-		SELECT e, m * $2 AS m FROM bag.j_as_t($1)
+		SELECT e, m * $2 AS m FROM bag.as_table($1)
 	) t
 $f$ language SQL IMMUTABLE;
-
 
 
 CREATE FUNCTION bag.intersection(JSONb[]) RETURNS JSONb AS $f$
 	SELECT jsonb_object_agg(e,m)
 	FROM (
 	  SELECT e, MIN(m) AS m
-		FROM bag.j_as_t($1)
+		FROM bag.as_table($1)
 		GROUP BY e
 		HAVING COUNT(*)=array_length($1,1)
 	) t
@@ -90,7 +108,7 @@ CREATE FUNCTION bag.union(JSONb[]) RETURNS JSONb AS $f$
 	SELECT jsonb_object_agg(e,m)
 	FROM (
 	  SELECT e, MAX(m) AS m
-		FROM bag.j_as_t($1)
+		FROM bag.as_table($1)
 		GROUP BY e
 	) t
 $f$ language SQL IMMUTABLE;
@@ -99,7 +117,7 @@ CREATE FUNCTION bag.sum(JSONb[]) RETURNS JSONb AS $f$
 SELECT jsonb_object_agg(e,m)
 FROM (
   SELECT e, SUM(m) AS m
-	FROM bag.j_as_t($1)
+	FROM bag.as_table($1)
 	GROUP BY e
 ) t
 $f$ language SQL IMMUTABLE;
@@ -131,7 +149,7 @@ $f$ language SQL IMMUTABLE;
 CREATE FUNCTION bag.is_sub(JSONb, JSONb) RETURNS boolean AS $f$
 -- compare performance with bag.is_sub($1,array[$2])
 	SELECT bool_and (CASE WHEN b.m IS NULL THEN false ELSE a.m<=b.m END)
-	FROM bag.j_as_t($1) a LEFT JOIN  bag.j_as_t($2) b ON a.e=b.e
+	FROM bag.as_table($1) a LEFT JOIN  bag.as_table($2) b ON a.e=b.e
 $f$ language SQL IMMUTABLE;
 
 /**
@@ -154,9 +172,9 @@ $f$ language SQL IMMUTABLE;
 
 
 /* tests:
-SELECT * FROM bag.j_as_t( '{"a":1,"b":3,"x":2,"z":1}'::jsonb );
+SELECT * FROM bag.as_table( '{"a":1,"b":3,"x":2,"z":1}'::jsonb );
 
-SELECT * FROM bag.j_as_t(array['{"a":1,"b":3,"x":2,"z":1}'::jsonb, '{"a":2,"b":1,"k":2,"m":1}'::jsonb]);
+SELECT * FROM bag.as_table(array['{"a":1,"b":3,"x":2,"z":1}'::jsonb, '{"a":2,"b":1,"k":2,"m":1}'::jsonb]);
 
  select bag.intersection( '{"a":1,"b":3,"x":2,"z":1}'::jsonb, '{"a":2,"b":1,"k":2,"m":1}'::jsonb  );
 
@@ -165,7 +183,7 @@ SELECT * FROM bag.j_as_t(array['{"a":1,"b":3,"x":2,"z":1}'::jsonb, '{"a":2,"b":1
  select * FROM bag.merge_as_table( '{"a":1,"b":3,"x":2,"z":1}'::jsonb, '{"a":2,"b":1,"k":2,"m":1}'::jsonb  ) t(k,v);
 
 -- rev bugs
- CREATE FUNCTION bag.j_as_t(JSONb) RETURNS SETOF bag.ref AS $f$
+ CREATE FUNCTION bag.as_table(JSONb) RETURNS SETOF bag.ref AS $f$
   --Returns a JSONb-bag representation as an element-multiplicity table.
  	DECLARE
  		aux JSONb;
@@ -173,7 +191,7 @@ SELECT * FROM bag.j_as_t(array['{"a":1,"b":3,"x":2,"z":1}'::jsonb, '{"a":2,"b":1
  	BEGIN
    IF jsonb_typeof($1)='array' THEN
  		SELECT array_agg(x) INTO aux FROM (SELECT jsonb_array_elements($1)) q(x);
- 		tt:= bag.j_as_t(aux);
+ 		tt:= bag.as_table(aux);
  	ELSE
  		SELECT key, (value#>>'{}')::int INTO tt FROM jsonb_each($1) a;
  	END IF;
